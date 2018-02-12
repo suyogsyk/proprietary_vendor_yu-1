f04a32f39a1569c078b6e25b2565174ef1a99bdc
Michael Bestas <mkbestas@lineageos.org>
2018-02-12 22:11:23 +0200

    lettuce: Add libcneapiclient and libsettings from OP3
    * Package version: OnePlus3Oxygen_16_OTA_036_all_1710262329_97651662219b4e6b
    
    Change-Id: Ie2374ef6c6f89f2ac404cf82e1e99bde7fa4d905

--- lettuce/lettuce-vendor.mk
+++ lettuce/lettuce-vendor.mk
@@ -179,6 +179,7 @@ PRODUCT_COPY_FILES += \
     vendor/yu/lettuce/proprietary/vendor/lib/soundfx/libqcbassboost.so:system/vendor/lib/soundfx/libqcbassboost.so \
     vendor/yu/lettuce/proprietary/vendor/lib/soundfx/libqcreverb.so:system/vendor/lib/soundfx/libqcreverb.so \
     vendor/yu/lettuce/proprietary/vendor/lib/soundfx/libqcvirt.so:system/vendor/lib/soundfx/libqcvirt.so \
+    vendor/yu/lettuce/proprietary/vendor/lib/vendor.qti.hardware.fm@1.0_vendor.so:system/vendor/lib/vendor.qti.hardware.fm@1.0_vendor.so \
     vendor/yu/lettuce/proprietary/vendor/lib64/com.quicinc.cne.api@1.0.so:system/vendor/lib64/com.quicinc.cne.api@1.0.so \
     vendor/yu/lettuce/proprietary/vendor/lib64/com.quicinc.cne.constants@1.0.so:system/vendor/lib64/com.quicinc.cne.constants@1.0.so \
     vendor/yu/lettuce/proprietary/vendor/lib64/egl/eglSubDriverAndroid.so:system/vendor/lib64/egl/eglSubDriverAndroid.so \
